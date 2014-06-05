f0ca7c4b0b1423723eeabf14e53d181656e52e03
Shuo Lv <claus.lv@hotmail.com>
2014-06-05 19:20:14 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+## plot 1
+source("import_data.R")
+filename <- "./Data/household_power_consumption.txt"
+
+data <- load_data(filename)
+
+png("plot1.png", width=400, height=400)
+        
+hist(data$Global_active_power,
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     col="red")
+        
+dev.off()
